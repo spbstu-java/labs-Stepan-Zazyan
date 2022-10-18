0a10ac5580674010342448c82c83140614b9b753
Stepan-Zazyan <stepanzazyan@yandex.ru>
2022-10-18 22:46:12 +0300

    Лабораторная работа: Translator

--- src/main/java/translator/Translator.java
+++ src/main/java/translator/Translator.java
@@ -7,53 +7,50 @@ import java.util.*;
 public class Translator {
     private final Map<String, String> dictionary = new HashMap<>();
 
-    public Map<String, String> dictionaryToMap(String path) {
+    public Map<String, String> dictionaryToMap(String path) throws FileNotFoundException {
         if (path == null || path.isBlank()) {
-            throw new RuntimeException();
+            throw new RuntimeException("Некорректный путь к словарю");
         }
         File file = new File(path);
         if (!file.exists() || !file.isFile() || !file.canRead()) {
-            throw new RuntimeException();
+            throw new RuntimeException("Проблема с открытием файла словаря");
         }
         try (Scanner scannerDictionary = new Scanner(file)) {
             while (scannerDictionary.hasNextLine()) {
                 String[] line = scannerDictionary.nextLine().split("\\|");
                 String key = line[0].trim().toLowerCase();
                 String value = line[1].trim().toLowerCase();
-                if (!dictionary.containsKey(key)) {
-                    dictionary.put(key, value);
+                for (Map.Entry<String, String> entry : dictionary.entrySet()) {
+                    if ((key.contains(entry.getKey()) || entry.getKey().contains(key))
+                            && key.length() > entry.getKey().length()) {
+                        key = entry.getKey();
+                    }
                 }
-                if (dictionary.get(key).length() < value.length()) {
-                    dictionary.replace(key, dictionary.get(key), value);
-                }
-
+                dictionary.put(key, value);
             }
-
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            throw new FileNotFoundException("Файл не найден во время создания мапы");
         }
         return dictionary;
     }
 
-    public String translateFile(String path) {
+    public String translateFile(String path) throws FileNotFoundException {
         if (path == null || path.isBlank()) {
-            throw new RuntimeException();
+            throw new RuntimeException("Некорректный путь к тексту для перевода");
         }
         File file = new File(path);
         if (!file.exists() || !file.isFile() || !file.canRead()) {
-            throw new RuntimeException();
+            throw new RuntimeException("Проблема с открытием текстового файла");
         }
         List<String> list = new ArrayList<>();
         try (Scanner scannerText = new Scanner(file)) {
             while (scannerText.hasNext()) {
                 list.add(scannerText.next().toLowerCase());
             }
-        } catch (FileNotFoundException ex) {
-            throw new RuntimeException(ex);
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("файл не найден во время переброса в строку");
         }
-
         String translated = String.join(" ", list).toLowerCase();
-
         for (Map.Entry<String, String> entry : dictionary.entrySet()) {
             translated = translated.replace(entry.getKey(), entry.getValue());
         }
