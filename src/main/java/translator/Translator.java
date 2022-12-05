1ed6a1145c31e15209acfb098ecdc65ae31ee2c0
Stepan-Zazyan <stepanzazyan@yandex.ru>
2022-12-05 12:16:17 +0300

    переводчик

--- src/main/java/translator/Translator.java
+++ src/main/java/translator/Translator.java
@@ -45,25 +45,15 @@ public class Translator {
             throw new FileNotFoundException("файл не найден во время переброса в строку");
         }
         String translated = "";
-        String str = "";
         for (int i = 0; i < list.size(); i++) {
-            if (i != list.size() - 1 && dictionary.containsKey(list.get(i) + " " + list.get(i + 1))) {
-                str += list.get(i) + " " + list.get(i + 1) + " ";
+            if (i != 0 && dictionary.containsKey(list.get(i - 1) + " " + list.get(i))) {
+                translated += dictionary.get(list.get(i - 1) + " " + list.get(i)) + " ";
                 i++;
-                if (i == list.size() - 1) {
-                    str = dictionary.get(str.trim());
-                }
-                for (int j = i + 1; j < list.size(); j++) {
-                    if (j != list.size() - 1 && dictionary.containsKey(str + list.get(j))) {
-                        str += list.get(j);
-                        i = j;
-                    } else {
-                        str = dictionary.get(str) + " ";
-                        break;
-                    }
-                }
-                translated += str;
-                str = "";
+                continue;
+            }
+            if (i != list.size() - 1 && dictionary.containsKey(list.get(i) + " " + list.get(i + 1))) {
+                translated += dictionary.get(list.get(i) + " " + list.get(i + 1)) + " ";
+                i++;
                 continue;
             }
             if (dictionary.containsKey(list.get(i))) {
